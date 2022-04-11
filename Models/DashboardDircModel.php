85ff971faa7cf09494663cda695ac116fc2a9059
JOSEPH1990a <JOSE_SANTIZR1990@HOTMAIL.COM>
2022-04-11 16:02:36 -0500

    terminando dirc con graficas

--- Models/DashboardDircModel.php
+++ Models/DashboardDircModel.php
@@ -3,12 +3,36 @@
         public function __construct(){
             parent::__construct();
         }
-        public function selectTotalesCard($plantel){
-            if($plantel == "all"){
-                $sqlPlanteles = "SELECT COUNT(*) AS total FROM t_planteles WHERE estatus = 1";
-                $requestPlanteles = $this->select($sqlPlanteles);
+
+
+        public function selectTotalPlanteles(string $nomConexion){
+            $sqlPlanteles = "SELECT COUNT(*) AS total FROM t_planteles";
+            $requestPlanteles = $this->select($sqlPlanteles,$nomConexion);
+            return $requestPlanteles;
+        }
+
+
+        public function selectTotalesPlanEstudios(string $nomConexion){
+            $sqlPlanEstudios = "SELECT COUNT(*) AS total FROM t_plan_estudios WHERE estatus = 1";
+            $requestPlanEstudios = $this->select($sqlPlanEstudios,$nomConexion);
+            return $requestPlanEstudios;
+        }
+        public function selectTotalesMaterias(string $nomConexion){
+            $sqlMaterias = "SELECT COUNT(*) AS total FROM t_materias WHERE estatus = 1";
+            $requestMaterias = $this->select($sqlMaterias, $nomConexion);
+            return $requestMaterias;
+        }
+        public function selectTotalesRVOES($nomConexion){
+            $sqlRVOES = "SELECT COUNT(*) AS total FROM t_plan_estudios WHERE DATEDIFF(fecha_actualizacion_rvoe,CURRENT_DATE) <= 365 AND estatus = 1";
+            $requestRVOES = $this->select($sqlRVOES, $nomConexion);
+            return $requestRVOES;
+        }
+/*         public function selectTotalesCard($nomConexion){
+            if($nomConexion == "all"){
+               $sqlPlanteles = "SELECT COUNT(*) AS total FROM t_db";
+                $requestPlanteles = $this->select($sqlPlanteles,'bd_usr');
                 $sqlPlanEstudios = "SELECT COUNT(*) AS total FROM t_plan_estudios WHERE estatus = 1";
-                $requestPlanEstudios = $this->select($sqlPlanEstudios);
+                $requestPlanEstudios = $this->select($sqlPlanEstudios,$nom);
                 $sqlMaterias = "SELECT COUNT(*) AS total FROM t_materias WHERE estatus = 1";
                 $requestMaterias = $this->select($sqlMaterias);
                 $sqlRVOES = "SELECT COUNT(*) AS total FROM t_plan_estudios WHERE DATEDIFF(fecha_actualizacion_rvoe,CURRENT_DATE) <= 365 AND estatus = 1";
@@ -30,40 +54,58 @@
                 $request['materias'] = $requestMaterias['total'];
                 $request['rvoes'] = $requestRVOES['total'];
                 $request['tipo'] = "";
-            }   
-            return $request;
+            }
+            return $requestPlanteles;
         }
-        public function selectRvoesExpirar($plantel){
+ */
+
+
+        public function selectRvoesExpirar(string $nomConexion,$plantel){
             if($plantel == "all"){
                 $sqlRVOES = "SELECT pl.id,pl.nombre_carrera,pl.nombre_carrera_corto,plnt.abreviacion_sistema,plnt.abreviacion_plantel,plnt.municipio,pl.rvoe,pl.fecha_actualizacion_rvoe FROM t_plan_estudios AS pl 
                 INNER JOIN t_planteles AS plnt ON pl.id_plantel = plnt.id WHERE DATEDIFF(pl.fecha_actualizacion_rvoe,CURRENT_DATE) <= 365 AND pl.estatus = 1";
-                $requestRVOES = $this->select_all($sqlRVOES);
+                $requestRVOES = $this->select_all($sqlRVOES,$nomConexion);
             }else{
                 $sqlRVOES = "SELECT pl.id,pl.nombre_carrera,pl.nombre_carrera_corto,plnt.abreviacion_sistema,plnt.abreviacion_plantel,plnt.municipio,pl.rvoe,pl.fecha_actualizacion_rvoe FROM t_plan_estudios AS pl INNER JOIN t_planteles AS plnt ON pl.id_plantel = plnt.id WHERE DATEDIFF(fecha_actualizacion_rvoe,CURRENT_DATE) <= 365 AND pl.id_plantel = $plantel AND pl.estatus = 1";
-                $requestRVOES = $this->select_all($sqlRVOES);
+                $requestRVOES = $this->select_all($sqlRVOES,$nomConexion);
             }
             return $requestRVOES;
         }
-        public function selectPlanteles(string $nomConexion){
-            $sql = "SELECT id,abreviacion_plantel,nombre_plantel,municipio FROM t_planteles WHERE estatus = 1";
+
+        //  Lista de Superplanteles
+        public function selectSuperplanteles(string $nomConexion){
+            $sql = "SELECT id,nombre_conexion,nombre_plantel FROM t_db";
             $request = $this->select_all($sql, $nomConexion);
             return $request;
         }
-        public function selectPlanEstudiosbyPlantel(int $idPlantel){
+
+
+        public function selectPlanEstudiosbyPlantel(string $nombreConexion,int $idPlantel){
             $sql = "SELECT COUNT(*) AS total FROM t_plan_estudios WHERE id_plantel = $idPlantel";
-            $request = $this->select($sql);
+            $request = $this->select($sql,$nombreConexion);
             return $request;
         }
-        public function selectMateriasbyPlantel(int $idPlantel){
+        public function selectMateriasbyPlantel(string $nombreConexion, int $idPlantel){
             $sql = "SELECT COUNT(*) AS total FROM t_materias AS mat
             INNER JOIN t_plan_estudios AS ples ON mat.id_plan_estudios = ples.id
-            INNER JOIN t_planteles AS pl ON ples.id_plantel = pl.id WHERE pl.id = $idPlantel";
-            $request = $this->select($sql);
+            INNER JOIN t_planteles AS pl ON ples.id_plantel = pl.id
+            WHERE pl.id = $idPlantel";
+            $request = $this->select($sql,$nombreConexion);
             return $request;
         }
-        public function selectRVOEproximoExpbyPlantel(int $idPlantel){
-            $sql = "SELECT COUNT(*) AS total FROM t_plan_estudios WHERE id_plantel = $idPlantel";
-            $request = $this->select($sql);
+        public function selectRVOEproximoExpbyPlantel(string $nombreConexion, int $idPlantel){
+            $sql = "SELECT pl.id,pl.nombre_carrera,pl.nombre_carrera_corto,plnt.abreviacion_sistema,plnt.abreviacion_plantel,plnt.municipio,pl.rvoe,pl.fecha_actualizacion_rvoe FROM t_plan_estudios AS pl INNER JOIN t_planteles AS plnt ON pl.id_plantel = plnt.id WHERE DATEDIFF(fecha_actualizacion_rvoe,CURRENT_DATE) <= 365 AND pl.id_plantel = $idPlantel AND pl.estatus = 1";
+            $request = $this->select_all($sql,$nombreConexion);
+            return $request;
+        }
+        public function selectDatosPlantel(string $nomConexion){
+            $sql = "SELECT *FROM t_planteles";
+            $request = $this->select_all($sql,$nomConexion);
+            return $request;
+        }
+        public function selectPlantel(string $nomConexion, int $idPlantel){
+            $sql = "SELECT *FROM t_planteles WHERE id = $idPlantel";
+            $request = $this->select($sql,$nomConexion);
             return $request;
         }
     }

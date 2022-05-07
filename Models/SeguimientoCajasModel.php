91ebf415e0efb2dc42d21c9c7e44e22c5b64e0f1
josesantiz-dev <JOSE_SANTIZR1990@HOTMAIL.COM>
2022-05-07 13:50:17 -0500

    modificacion en seguimiento de cajas

--- Models/SeguimientoCajasModel.php
+++ Models/SeguimientoCajasModel.php
@@ -5,14 +5,14 @@
         }
         public function selectCajeros(string $nomConexion){
             $sql = "SELECT ca.id AS id_caja, us.id AS id_usuario, per.id AS id_persona,ca.nombre AS nombre_caja,
-            pl.nombre_plantel, pl.nombre_sistema,per.nombre_persona,per.ap_paterno,per.ap_materno,ec.estatus_caja FROM t_cajas AS ca 
+            per.nombre_persona,per.ap_paterno,per.ap_materno,ec.estatus_caja FROM t_cajas AS ca 
             INNER JOIN t_usuarios AS us ON ca.id_usuario_atiende = us.id
-            INNER JOIN t_planteles AS pl ON ca.id_plantel = pl.id
             INNER JOIN t_personas AS per ON us.id_persona = per.id
             INNER JOIN t_estatus_caja AS ec ON ec.id_caja = ca.id";
             $request = $this->select_all($sql,$nomConexion);
             return $request;
         }
+
         public function selectCaja(int $idCaja, string $nomConexion){
             $sql = "SELECT c.id,c.nombre,ec.estatus_caja,cc.id AS id_corte_caja,cc.fechayhora_apertura_caja,cc.fechayhora_cierre_caja FROM t_cajas AS c 
             INNER JOIN t_estatus_caja AS ec ON ec.id_caja = c.id
